8dc0c7f9ded511088a2ff47de4273537a414c802
ashifmahmud <61384441+ashifmahmud@users.noreply.github.com>
2021-07-09 01:14:08 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-
-//Promt to select media stream, pass to video element, then play
-
-async function selectMediaStream(){
-
-  try{
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = mediaStream;
-    videoElement.onloadedmetadata = () =>{
+    videoElement.onloadedmetadata = () => {
       videoElement.play();
-    }
-  }
-  catch(error){
-    //Catch our error
-    console.log('whoops!',error);
+    };
+  } catch (error) {
+    // Catch Error Here
   }
 }
 
-button.addEventListener('click',async () =>{
-  // Disable the button
-
+button.addEventListener('click', async () => {
+  // Disable Button
   button.disabled = true;
-
-  //start picture in picture
-
+  // Start Picture in Picture
   await videoElement.requestPictureInPicture();
-
+  // Reset Button
   button.disabled = false;
 });
 
+// On Load
 selectMediaStream();
